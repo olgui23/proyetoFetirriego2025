de1ec11ecb64eaeb49ce8ab0d28c9a6b77154194
Olgui23 <olguinaamaya@gmail.com>
2025-06-19 18:46:59 -0400

    Actualización de tabla usuarios

--- database/migrations/2025_05_29_233141_create_usuarios_table.php
+++ database/migrations/2025_05_29_233141_create_usuarios_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('email')->unique();
+            $table->string('rol'); 
             $table->timestamps();
         });
     }
